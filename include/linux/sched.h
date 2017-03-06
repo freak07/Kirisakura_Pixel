6937c79476522e917a6aebb1e52b1dc433427c4a
freak07 <michalechner92@googlemail.com>
2017-03-06 11:27:45 +0100

    Revert "ANDROID: sched: add a counter to track fsync"
    This reverts commit 6c5b66e4a91f156f5454ab2842c502bb4b2ec13e.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3155,11 +3155,6 @@ static inline void inc_syscw(struct task_struct *tsk)
 {
 	tsk->ioac.syscw++;
 }
-
-static inline void inc_syscfs(struct task_struct *tsk)
-{
-	tsk->ioac.syscfs++;
-}
 #else
 static inline void add_rchar(struct task_struct *tsk, ssize_t amt)
 {
@@ -3174,9 +3169,6 @@ static inline void inc_syscr(struct task_struct *tsk)
 }
 
 static inline void inc_syscw(struct task_struct *tsk)
-{
-}
-static inline void inc_syscfs(struct task_struct *tsk)
 {
 }
 #endif
